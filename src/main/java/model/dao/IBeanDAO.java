2ef36315ee45d1f1376e31abb598c3e77e87c9ef
Arakno2100 <162506213+Arakno2100@users.noreply.github.com>
2024-05-29 13:02:32 +0200

    Introduzione classi DAO

---
+++ src/main/java/model/dao/IBeanDAO.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface IBeanDAO<T> {
+
+    public void doSave(T bean) throws SQLException;
+
+    public boolean doDelete(int id) throws SQLException;
+
+    public T doRetrieveByKey(int id) throws SQLException;
+
+    public Collection<T> doRetrieveAll(String order) throws SQLException;
+}
