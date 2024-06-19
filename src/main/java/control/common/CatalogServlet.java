a0d0767a17ced29a777f71e0570f9b70e4966107
Arakno2100 <162506213+Arakno2100@users.noreply.github.com>
2024-06-19 19:25:43 +0200

    Migliorata leggibilità del codice

--- src/main/java/control/common/CatalogServlet.java
+++ src/main/java/control/common/CatalogServlet.java
@@ -21,43 +21,48 @@ public class CatalogServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
-
         DataSource dataSource = (DataSource) getServletContext().getAttribute("DataSource");
         prodottoDao = new ProdottoDao(dataSource);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            // Recupera tutti i prodotti
+        try
+        {
             List<ProdottoBean> products = (List<ProdottoBean>) prodottoDao.doRetrieveAll("id");
-            int priceAttribute = Integer.MAX_VALUE;
 
-            try
-            {
-                priceAttribute = Integer.parseInt(request.getParameter("maxPrice"));
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
+            int maxPrice = extractMaxPrice(request);
 
-            if (priceAttribute <= 0)
-                priceAttribute = Integer.MAX_VALUE;
+            products = filterProductsByPrice(products, maxPrice);
 
-            final int  maxPrice = priceAttribute;
-
-            //Filtrare i prodotti da mostrare
-            products = products.stream().filter(product -> product.getPrezzo() <= maxPrice).collect(Collectors.toList());
-
-            // Passa i dati recuperati come attributi alla richiesta
             request.setAttribute("products", products);
 
-            // Inoltra la richiesta alla JSP
             request.getRequestDispatcher("/common/catalog.jsp").forward(request, response);
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new ServletException("Errore durante il recupero dei dati dal database", e);
         }
+    }
+
+    private int extractMaxPrice(HttpServletRequest request) {
+        int maxPrice;
+
+        try
+        {
+            maxPrice = Integer.parseInt(request.getParameter("maxPrice"));
+        }
+        catch (NumberFormatException e)
+        {
+            maxPrice = Integer.MAX_VALUE;
+        }
+
+        return maxPrice > 0 ? maxPrice : Integer.MAX_VALUE;
+    }
+
+    private List<ProdottoBean> filterProductsByPrice(List<ProdottoBean> products, int maxPrice) {
+        return products.stream()
+                .filter(product -> product.getPrezzo() <= maxPrice)
+                .collect(Collectors.toList());
     }
 }
