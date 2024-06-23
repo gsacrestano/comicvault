32c0364eed97040b3364834bb1c3afcd4690ca39
gsacrestano <g.sacrestano@studenti.unisa.it>
2024-06-23 18:18:54 +0200

    ajax get out

--- src/main/webapp/scripts/ajaxScript.js
+++ src/main/webapp/scripts/ajaxScript.js
@@ -1,10 +1,9 @@
-function sendSliderValue() {
+function sendSliderValue(contextPath) {
     const xhr = new XMLHttpRequest();
     const timestamp = new Date().getTime(); // Aggiungi un timestamp per evitare il caching
     const searchValue = document.getElementById('search').value; // Assumi che ci sia un input con id="search"
-    const str = `/common/NumProductServlet?name=` + searchValue;
+    const str = `${contextPath}/common/NumProductServlet?name=` + searchValue + "&timestamp=" + timestamp;
     xhr.open('GET', str, true);
-
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
